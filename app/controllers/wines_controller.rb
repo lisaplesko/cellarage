79462c420139e0c034be235ef0f3836b80887346
Lisa Plesko <plesko.l@gmail.com>
2014-05-20 15:37:24 -0400

    Syntax fix

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -1,5 +1,5 @@
 class WinesController < ApplicationController
-  before_action :authenticate_user!, except [:index, :show]
+  before_action :authenticate_user!, except; [:index, :show]
 
 
   def index
