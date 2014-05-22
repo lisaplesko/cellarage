5a6a95ec59a86e60550053d456a029d22fbf6d5f
Lisa Plesko <plesko.l@gmail.com>
2014-05-22 19:11:22 -0400

    Clean code & comments

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -58,42 +58,12 @@ class Wine < ActiveRecord::Base
   ## Search fields for sorting on index page. Could be more DRY!
 
   def self.by(query_params)
-#      {category: "kdjdfkjf"}
-#      {grape: "kdjdfkjf"}
-#      {occasion: "kdjdfkjf"}
+# Query params will come in like the following:
+#      {category: "Red"}
+#      {grape: "Cabernet"}
+#      {occasion: "Celebration"}
     Wine.where(query_params).order(:name)
   end
 
-  # def self.by_category(category)
-  #   if category
-  #     Wine.order(:name).where(category: category)
-  #   else
-  #     Wine.order(:name)
-  #   end
-  # end
-
-  # def self.by_varietal(grape)
-  #   if grape
-  #     Wine.order(:name).where(grape: grape)
-  #   else
-  #     Wine.order(:name)
-  #   end
-  # end
-
-  # def self.by_occasion(occasion)
-  #   if occasion
-  #     Wine.order(:name).where(occasion: occasion)
-  #   else
-  #     Wine.order(:name)
-  #   end
-  # end
-
-   # def self.by_category(search_item)
-  #   if search_item
-  #     Wine.order(:name).where(category: search_item)
-  #   else
-  #     Wine.order(:name)
-  #   end
-  # end
 
 end
